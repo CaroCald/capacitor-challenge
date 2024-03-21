2f84b3ac98b66e3349b7e6480132efb950af6a2e
Carolina Calderon <carolina.calderon@bayteq.com>
2024-03-21 17:54:27 -0500

    init geo location

--- heroes-app-main/src/app/auth/pages/login-page/login-page.component.ts
+++ heroes-app-main/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { Geolocation } from '@capacitor/geolocation';
+import { Capacitor } from '@capacitor/core';
 
 @Component({
   selector: 'app-login-page',
@@ -19,11 +21,36 @@ export class LoginPageComponent {
 
     this.authService.login('fernando@gmail.com','123456')
       .subscribe( user => {
-
+        this.checkPermissions()
         this.router.navigate(['/']);
 
       });
 
   }
 
+  async checkLocation(){
+    const coordinates = await Geolocation.getCurrentPosition();
+    console.log('Current position: ', coordinates);
+  }
+  
+
+   async checkPermissions(){
+    if (Capacitor.isNativePlatform()) {
+      const permission = await Geolocation.requestPermissions()
+      console.log(permission)
+      if(permission){
+        if( permission.coarseLocation == 'granted' && permission.location == 'granted'){
+          this.checkLocation()
+        } else if (permission.coarseLocation == 'denied' && permission.location == 'denied'){
+          alert("User Deny Location Access Permission")
+        }
+      }else{
+        alert("User Deny Location Access Permission")
+        
+      }
+    }else{
+      this.checkLocation()
+    }
+   
+  }
 }
